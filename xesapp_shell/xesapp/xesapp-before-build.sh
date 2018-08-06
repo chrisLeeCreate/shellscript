ed306ef2a1bed9f53c16965c4d9497b98b94da70
lishaowei <lishaowei@boxfish.cn>
2018-08-06 11:35:24 +0800

    update

--- xesapp_shell/xesapp/xesapp-before-build.sh
+++ xesapp_shell/xesapp/xesapp-before-build.sh
@@ -1,6 +1,28 @@
 #sdk路径
 cp /Users/xesapp/.jenkins/workspace/android-enviroment/local.properties /Users/xesapp/.jenkins/workspace/android-xesApp
 
+
+#检测当前分支是否合并了master分支代码
+if [ $enviroment == "Release" ]
+then
+	git pull origin master:master
+	git pull origin $branch:$branch
+	UnmergeCommit="./UnmergeCommit"
+	git log master ^$branch >$UnmergeCommit
+	commitLog=`cat $UnmergeCommit`
+	echo $commitLog
+	if [ ! -z "$commitLog" ];
+	then
+  		echo "master分支的代码尚未合并至该分支"
+ 	    rm -rf $UnmergeCommit
+  		#强行停止
+  		master分支的代码尚未合并至该分支 
+  		exit
+	else
+ 		rm -rf $UnmergeCommit
+	fi
+fi
+
 #删掉临时build缓存
 cd app
 rm -rf build
